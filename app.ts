4800b43c1b286f3cf98a0d1e288ee7946c1ced72
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-10-29 12:21:09 -0300

    add logging middleware

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import launchesRouter from "./controllers/launches";
+import middlewares from "./utils/middlewares";
 
 const app = express();
+app.use(middlewares.requestLogger);
 
 app.use("/launches", launchesRouter);
 
