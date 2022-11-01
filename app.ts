bb1dd16d10dab698a610497ad8a1f95c44e56711
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-11-01 11:43:24 -0300

    add cors

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import launchesRouter from "./controllers/launches";
 import middlewares from "./utils/middlewares";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(middlewares.requestLogger);
 
 app.use("/launches", launchesRouter);
